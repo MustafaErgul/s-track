322716cca78174c830ce153ce3a05c3ebd65bfb6
Mustafa Ergül <mustafaergul0@gmail.com>
2020-12-18 13:12:53 +0300

    remove debug line

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -4,7 +4,6 @@ class UserStocksController < ApplicationController
     stock = Stock.check_db(params[:ticker])
     if stock.blank?
       stock = Stock.get_stock_price(params[:ticker])
-      p stock.inspect
       stock["ticker"].save
     end
     @user_stock = UserStock.create(user: current_user, stock: stock)
