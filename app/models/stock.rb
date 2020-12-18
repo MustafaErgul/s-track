520abc27f7184473e1ba2d146bc10c78378d7e6d
Mustafa Ergül <mustafa@Mustafa-MacBook-Pro.local>
2020-12-18 13:03:10 +0300

    author set

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
   has_many :user_stocks, :dependent => :destroy
-  has_many :user, through: :user_stocks
+  has_many :users, through: :user_stocks
 
   validates :name, :ticker, presence: true
 
@@ -28,10 +28,13 @@ class Stock < ApplicationRecord
   end
 
   def self.get_stock_price(symbol)
-
     info = get_stock_info(symbol)
     info["last"]
   end
 ############################################
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
 
 end
