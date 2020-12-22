6aa11fb287d2904fa22219e283bed45190723ca4
Mustafa Ergül <mustafaergul0@gmail.com>
2020-12-22 13:40:44 +0300

    Fix result and exception handling

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -3,8 +3,8 @@ class UserStocksController < ApplicationController
   def create
     stock = Stock.check_db(params[:ticker])
     if stock.blank?
-      stock = Stock.get_stock_price(params[:ticker])
-      stock["ticker"].save
+      stock = Stock.get_stock_info(params[:ticker])
+      stock.save
     end
     @user_stock = UserStock.create(user: current_user, stock: stock)
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -11,16 +11,18 @@ class Stock < ApplicationRecord
     uri = URI("https://api.tiingo.com/iex/?tickers=" + symbol)
     req = Net::HTTP::Get.new(uri)
 
+    headers.each { |key, value| req[key.to_s] = value}
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    res = http.request(req)
+    response = JSON.parse(res.body)
+
+    response.first
+
     begin
-      headers.each { |key, value| req[key.to_s] = value}
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-
-      res = http.request(req)
-      response = JSON.parse(res.body)
-
-      response.first
+      new(ticker: symbol, last_price: response.first["last"])
     rescue => exception
       return nil
     end
